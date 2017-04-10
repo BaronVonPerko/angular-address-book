d42b88f518bf9eaa54e72133f1a6359e4f109bb5
cperko <cperko@teachers-teachers.com>
2017-04-10 12:47:11 -0500

    init from end of part2

---
+++ Part3Practice2/src/app/firebase-config-example.ts
@@ -0,0 +1,9 @@
+export class FirebaseConfigExample { // Raname to FirebaseConfig
+    static myFirebaseConfig = {
+        apiKey: '<your-key>',
+        authDomain: '<your-project-authdomain>',
+        databaseURL: '<your-database-URL>',
+        storageBucket: '<your-storage-bucket>',
+        messagingSenderId: '<your-messaging-sender-id>'
+    };
+}
